76be6f5807f3b582cc6883007bac8f2ca14e168f
Taylor B <tayjaybabee@gmail.com>
2023-10-10 16:30:09 -0500

    refactor

--- sense_service/errors.py
+++ sense_service/errors.py
@@ -5,6 +5,6 @@ Contains custom exceptions for SenseService
 
 class InvalidTemperatureSourceError(Exception):
     message = 'Invalid value for "temperature_source" must be one of; "pressure", or "humidity"!'
+
     def __init__(self, *args, **kwargs):
         super().__init__(self.message)
-
